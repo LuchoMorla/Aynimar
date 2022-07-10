340b09ed69994f6b7b3a889c97708251a78f5fdf
morlucho <luis-fernando-10@outlook.es>
2022-07-10 03:19:44 -0500

    update categories and waste categories permisions

--- routes/wasteCategoriesRouting.js
+++ routes/wasteCategoriesRouting.js
@@ -46,7 +46,7 @@ router.get(
 router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
-  checkRoles('admin'),
+  checkRoles('admin', 'recycler'),
   passport.authenticate('jwt', { session: false }),
   validatorHandler(createWasteCategorySchema, 'body'),
   async (req, res, next) => {
