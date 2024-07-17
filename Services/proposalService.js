121bfd6d114e8fe1e6ecf3a670eda458533d93c0
alejooroncoy <adoa2705@gmail.com>
2024-07-17 08:08:08 -0500

    fix(dev-dashboard): Proposal

--- Services/proposalService.js
+++ Services/proposalService.js
@@ -22,14 +22,26 @@ class ProposalService {
   async sendProposal(email, name, proposal) {
     const mail = {
       from: config.smtpMail, // sender address
-      to: config.proposalEmailRmail || config.smtpMail, // list of receivers
+      to: config.receivermail || config.smtpMail, // list of receivers
       subject: `Propuesta enviada por ${name}`, // Subject line
       html: `<p>${name} con correo "${email}" ha enviado una propuesta.</p>
-        <br />
         <p>La propuesta fue la siguiente: ${proposal}</p>`, // html body
     }
+
+    const mailConfirmation = {
+      from: config.smtpMail, // sender address
+      to: `${email}`, // list of receivers
+      subject: `Confirmación de propuesta a Aynimar`, // Subject line
+      html: `<p>¡Hola, ${name}!.</p>
+        <p>Tu propuesta ha sido recibida por el equipo, muchas gracias por su recomendación a nuestra plataforma.</p>`, // html body
+    }
+
     const rta = await this.sendMail(mail);
-    return rta;
+    const rtaConfirmation = await this.sendMail(mailConfirmation);
+    return {
+      rta,
+      rtaConfirmation
+    };
   }
 
 }
