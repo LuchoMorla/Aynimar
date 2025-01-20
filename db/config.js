e24487e8c93415996c08df25a2529fff29b1f409
alejooroncoy <adoa2705@gmail.com>
2025-01-20 17:52:09 -0500

    fix: Fix errors

--- db/config.js
+++ db/config.js
@@ -11,7 +11,7 @@ module.exports = {
     development: {
         url: config.dbUrl,
         dialect: 'postgres',
-        dialectOptions: { ssl: { require: true } },
+        // dialectOptions: { ssl: { require: true } },
     },
     production: {
         url: config.dbUrl,
